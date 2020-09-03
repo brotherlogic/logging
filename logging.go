a2027c2ed61d4c059b1929e624c2271e15d0a818
Simon Tucker <brotherlogic@gmail.com>
2020-09-03 08:02:15 -0700

    Bouncing

--- logging.go
+++ logging.go
@@ -74,7 +74,9 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{}
+	return []*pbg.State{
+		&pbg.State{Key: "blah", Value: int64(12)},
+	}
 }
 
 func (s *Server) marshal(logs []*pb.Log) ([]byte, error) {
