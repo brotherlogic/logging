6bea6afc5702f1039e7856cdfd6879a4785b8faf
Simon Tucker <brotherlogic@gmail.com>
2022-01-15 15:01:55 -0800

    Push parsing out of loop

--- logging_cli/cli.go
+++ logging_cli/cli.go
@@ -28,40 +28,40 @@ func main() {
 		log.Fatalf("Error finding logging servers: %v", err)
 	}
 
-	logs := []*pb.Log{}
-	var wg sync.WaitGroup
-	for _, server := range servers {
-		wg.Add(1)
-		go func(serv string) {
-			defer wg.Done()
-			ctx, cancel := utils.ManualContext("logging-cli", time.Minute)
-			defer cancel()
-			conn, err := utils.LFDial(serv)
-			if err != nil {
-				fmt.Printf("Dial error: %v", err)
-				return
-			}
-			defer conn.Close()
-			client := pb.NewLoggingServiceClient(conn)
+	if err := logFlags.Parse(os.Args[2:]); err == nil {
 
-			var res *pb.GetLogsResponse
+		logs := []*pb.Log{}
+		var wg sync.WaitGroup
+		for _, server := range servers {
+			wg.Add(1)
+			go func(serv string) {
+				defer wg.Done()
+				ctx, cancel := utils.ManualContext("logging-cli", time.Minute)
+				defer cancel()
+				conn, err := utils.LFDial(serv)
+				if err != nil {
+					fmt.Printf("Dial error: %v", err)
+					return
+				}
+				defer conn.Close()
+				client := pb.NewLoggingServiceClient(conn)
 
-			if err := logFlags.Parse(os.Args[2:]); err == nil {
+				var res *pb.GetLogsResponse
+
 				res, err = client.GetLogs(ctx, &pb.GetLogsRequest{Origin: os.Args[1], Match: *matcher, IncludeDlogs: *include, Context: *context})
 				if err == nil {
 					logs = append(logs, res.GetLogs()...)
 				}
-			}
-		}(server)
-	}
+			}(server)
+		}
+		wg.Wait()
 
-	wg.Wait()
+		sort.SliceStable(logs, func(i, j int) bool {
+			return logs[i].GetTimestamp() > logs[j].GetTimestamp()
+		})
 
-	sort.SliceStable(logs, func(i, j int) bool {
-		return logs[i].GetTimestamp() > logs[j].GetTimestamp()
-	})
-
-	for _, l := range logs {
-		fmt.Printf("%v - %v\n", time.Unix(l.GetTimestamp(), 0), l)
+		for _, l := range logs {
+			fmt.Printf("%v - %v\n", time.Unix(l.GetTimestamp(), 0), l)
+		}
 	}
 }
