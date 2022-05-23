b78e00ff8cbd7a356eaf1e026ed7667f43988ff3
Simon Tucker <brotherlogic@gmail.com>
2022-05-23 12:44:19 -0700

    Include error in log

--- loggingutils.go
+++ loggingutils.go
@@ -86,7 +86,7 @@ func (s *Server) loadAllLogs(ctx context.Context, origin string, match string, i
 	// Walk the dlogs if we've been asked to
 	if includeDLogs {
 		err := filepath.Walk(fmt.Sprintf("%v/%v", s.dpath, origin), func(path string, info os.FileInfo, err error) error {
-			s.CtxLog(ctx, fmt.Sprintf("Huh %v, %v, %v", origin, path, info))
+			s.CtxLog(ctx, fmt.Sprintf("Huh %v, %v, %v, %v", origin, path, info, err))
 			if (origin == "" || strings.Contains(path, origin)) && !info.IsDir() {
 				dlogs, err := s.loadDLogFile(path, origin, context)
 				if err != nil {
