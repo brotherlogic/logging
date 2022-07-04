fd5a839a41a28b8f66d921e57c6c9070586404b0
Simon Tucker <brotherlogic@gmail.com>
2022-07-04 13:04:49 -0700

    Include hard timestamp

--- loggingutils.go
+++ loggingutils.go
@@ -138,7 +138,7 @@ func (s *Server) cleanAllLogs() error {
 			newlogs := []*pb.Log{}
 			for _, log := range nlogs {
 				if time.Since(time.Unix(log.GetTimestamp(), 0)).Seconds() < float64(log.GetTtl()) {
-					s.DLog(context.Background(), fmt.Sprintf("TTL: %v -> %v", log.GetTtl(), time.Since(time.Unix(log.GetTimestamp(), 0)).Seconds()))
+					s.DLog(context.Background(), fmt.Sprintf("TTL: %v -> %v from (%v)", log.GetTtl(), time.Since(time.Unix(log.GetTimestamp(), 0)).Seconds(), time.Unix(log.GetTimestamp(), 0)))
 					newlogs = append(newlogs, log)
 				}
 			}
