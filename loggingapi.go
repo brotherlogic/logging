d51bcb2840eef780c443e8a636aaf68a27e9d8c4
brotherlogic <brotherlogic@gmail.com>
2025-07-14 06:44:55 -0700

    Adds log size check

--- loggingapi.go
+++ loggingapi.go
@@ -13,18 +13,23 @@ var (
 		Name: "logging_requests",
 		Help: "The size of the logs",
 	}, []string{"origin"})
+
+	logSize = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "logging_logsize",
+	}, []string{"orgin"})
 )
 
-//Log logs a message
+// Log logs a message
 func (s *Server) Log(ctx context.Context, req *pb.LogRequest) (*pb.LogResponse, error) {
 	request.With(prometheus.Labels{"origin": req.GetLog().GetOrigin()}).Inc()
 	logs, err := s.loadLogs(ctx, req.GetLog().GetOrigin(), req.GetLog().GetTimestamp())
 	logs = append(logs, req.GetLog())
+	logSize.With(prometheus.Labels{"origin": req.GetLog().GetOrigin()}).Set(float64(len(logs)))
 	err = s.saveLogs(ctx, req.GetLog().GetOrigin(), req.GetLog().GetTimestamp(), logs)
 	return &pb.LogResponse{}, err
 }
 
-//GetLogs gets the logs
+// GetLogs gets the logs
 func (s *Server) GetLogs(ctx context.Context, req *pb.GetLogsRequest) (*pb.GetLogsResponse, error) {
 	logs, err := s.loadAllLogs(ctx, req.GetOrigin(), req.GetMatch(), req.GetIncludeDlogs(), req.GetContext())
 	return &pb.GetLogsResponse{Logs: logs}, err
